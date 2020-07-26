68b4e1b368049cb98410c267c367c2a69a53f250
slickben <benson.isaac.momodu@gmail.com>
2020-07-26 08:53:33 +0100

    hhhh

---
+++ js/three-grid-column.js
@@ -0,0 +1,15 @@
+// $(".cart-text").hide()
+
+
+
+
+// $(document).ready(function(){
+//     $(".cart-icon").mouseenter(function(){
+//         $(".cart-text").show()
+//     });
+//     $("#cart-icon").mouseleave(function(){
+//         $(".cart-text").hide()
+//     });
+// });
+$(".cart-text").hide()
+
